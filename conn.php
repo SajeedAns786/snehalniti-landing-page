27f66180086fa5013ea0837c5c82a82b31be6a55
Sajeed Ansari <102681838+SajeedAns786@users.noreply.github.com>
2023-08-25 12:37:45 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost","snehalnitimain_one","r+Fp{S1Y9s8+","snehalnitimain_one");
+$conn = mysqli_connect("localhost","","","");
 mysqli_set_charset($conn,"utf8");
 
 if (!$conn) {
