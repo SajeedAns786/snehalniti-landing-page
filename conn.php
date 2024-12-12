a1ffe15c114df7e98ac3a81fecf1a68035b6927b
Sajeed Ansari <102681838+SajeedAns786@users.noreply.github.com>
2024-12-12 17:06:31 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
 $conn = mysqli_connect("localhost","","","");
 mysqli_set_charset($conn,"utf8");
-
+asdfghjkliouytresdfxcvbnm
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
